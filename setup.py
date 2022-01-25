33404d4b4ad09ea1bf2c51d9fc7af455956b2cd9
Alan Chen <github@zenan.ch>
2022-01-24 22:28:38 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="canvas-tools",
-    version="0.0.3",
+    version="0.0.4",
     author="Alan Chen",
     author_email="me@zenan.ch",
     description="A set of tools for working with Canvas.",
